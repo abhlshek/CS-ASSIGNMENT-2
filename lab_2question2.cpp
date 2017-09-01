031140ad23f193338214f8fcd56ac2b320be9d9f
ABHISHEK RAJ <31350671+abhlshek@users.noreply.github.com>
2017-09-01 22:29:46 +0530

    Create lab_2question2.cpp

---
+++ lab_2question2.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+float c;
+cout<<"enter temperature in celcius"<<"\n";
+cin>>c;
+float f=c*9/5+32;
+cout<<"temp in farenheit is "<<f<<"\n";
+return(0);
+}
+
